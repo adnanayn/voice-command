6a1781d7e6094e762ecd0ea498beb926440fcf97
Yasir Adnan <adnan.ayon@gmail.com>
2012-08-04 20:20:28 +0600

    Revert "Updated"
    This reverts commit f0bc23f45c0f5742973b14deaa3d5394412bff90.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,7 +16,6 @@ using System.Diagnostics;//Process
 using System.Speech.Synthesis;//Speaking
 using System.Windows.Forms;
 using System.Drawing;
-using System.Text.RegularExpressions;
 
 namespace VoiceApp
 {
@@ -46,7 +45,7 @@ namespace VoiceApp
                 Speech.SetInputToDefaultAudioDevice(); // Set Deafault microphone to listen voice
 
                 gmBuilder(); // Grammar Loader
-              // anotherGm();
+               anotherGm();
             
                 Speech.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(_Speech_SpeechRecognized);
 
@@ -62,68 +61,45 @@ namespace VoiceApp
             foreach (RecognizedWordUnit word in e.Result.Words)
             {
                 //inputfield.Items.Add(word.Text);
+                if (word.Confidence > 0.0f)
+                {
                     inputfield.Items.Add(word.Text);
                     switch (word.Text)
                     {
-                        case "MyComputer":
-                            DeviceSpeak.Speak("Opening My Computer");
-                            Process.Start("::{20d04fe0-3aea-1069-a2d8-08002b30309d}");
-                            break;
-                        
                         case "FireFox":
                             DeviceSpeak.Speak("Starting Firefox");
-                            currentProcess = Process.Start("firefox.exe");
+                            Process.Start("firefox.exe");
                             break;
 
                         case "Notepad":
                             DeviceSpeak.Speak("Starting Notepad");
                             currentProcess = Process.Start("Notepad.exe");
                             break;
-                        
-                        case "NewTab":
+
+                        case "Test":
+                            currentProcess = Process.Start("http://yasiradnan.com");
+                            DeviceSpeak.Speak("" + currentProcess);
+                            /*DeviceSpeak.Speak("Starting uTorrent");
+                                Process.Start("uTorrent.exe");*/
+                            break;
+                        case "New Tab":
                             DeviceSpeak.Speak("Opening New Tab");
                             SendKeys.SendWait("^{T}");
                             break;
-                        case "ControlPanel":
-                            DeviceSpeak.Speak("Opening Control Panel");
-                            currentProcess = Process.Start("::{21EC2020-3AEA-1069-A2DD-08002b30309d}");
-                            break;
-                            
-                        case "NetworkPlaces":
-                            DeviceSpeak.Speak("Opening Network Places");
-                            currentProcess = Process.Start("::{208d2c60-3aea-1069-a2d7-08002b30309d}");
-                            break;
-                        case "StartMenu" :
-                            DeviceSpeak.Speak("Opening Start Menu");
-                           // currentProcess = Process.Start("::{48e7caab-b918-4e58-a94d-505519c795dc}");
-                            break;
-                        case "RecycleBin" :
-                            DeviceSpeak.Speak("Opening Recycle Bin");
-                            currentProcess = Process.Start("::{645FF040-5081-101B-9F08-00AA002F954E}");
-                            break;
-                        case "MyDocuments" :
-                            DeviceSpeak.Speak("Opening My Documents");
-                            currentProcess = Process.Start("::{450D8FBA-AD25-11D0-98A8-0800361B1103}");
-                            break;
-                        case "AdministrativeTools" :
-                            DeviceSpeak.Speak("Opening My Documents");
-                            currentProcess = Process.Start("::{D20EA4E1-3957-11d2-A40B-0C5020524153}");
-                            break;
-                        case "TaskManager":
-                            DeviceSpeak.Speak("Opening Task Manager");
-                            currentProcess = Process.Start("^+{ESC}");
-                            break;
+
                         case "Close":
+                            //DeviceSpeak.Speak("Closing Application" + currentProcess.CloseMainWindow());
                             DeviceSpeak.Speak("Closing Application");
+                            //currentProcess.CloseMainWindow();
                             SendKeys.SendWait("%{F4}");
                             break;
-                        
+
                         default:
                             //DeviceSpeak.Speak("Searching"+word.Text);
                             //currentProcess = Process.Start("http://google.com/search?q="+word.Text);
                             break;
                     }
-                
+                }
 
             }
 
@@ -139,7 +115,7 @@ namespace VoiceApp
         private void gmBuilder()
         {
             Choices cmd = new Choices();
-            cmd.Add(new string[] {"Close", "FireFox", "NewTab","Notepad","MyComputer","ControlPanel","NetworkPlaces","StartMenu","RecycleBin","MyDocuments","Administrative Tools","TaskManager" });
+            cmd.Add(new string[] {"Test", "Close", "FireFox", "New Tab","Notepad" });
 
             /*Create a GrammarBuilder object and append the Choices object.*/
             GrammarBuilder gb = new GrammarBuilder();
@@ -155,11 +131,14 @@ namespace VoiceApp
             Speech.LoadGrammar(new DictationGrammar());
 
         }
-        private void MoveMouse(int x, int y)
+        private void MouseMove(int x, int y)
         {
-            /*this.Cursor = new System.Windows.Input.Cursor(Cursor.Current.Handle);
-            Cursor.Position = new Point(x, y);
-            Cursor.Clip = new Rectangle(this.Location, this.Size);*/
+            /*Moving Mouse*/
+           
+
+            
+
+
         }
     }
 }
